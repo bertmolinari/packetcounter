0f54c82cf539c1230a91f0dbdd0249659127fbf0
bertmolinari <bert.molinari@gmail.com>
2018-12-29 21:03:52 -0800

    - setting the socket setup to bind to any address on the machine, not just localhost

--- packetcounter.c
+++ packetcounter.c
@@ -156,13 +156,15 @@ void* packetListener(void* param)
     /* 1) Internet domain 2) Stream socket 3) Default protocol (TCP in this case) */
     welcomeSocket = socket(PF_INET, SOCK_STREAM, 0);
 
+    /* get the IP addresses associatedw ith this machine
     /*---- Configure settings of the server address struct ----*/
     /* Address family = Internet */
     serverAddr.sin_family = AF_INET;
     /* Set port number, using htons function to use proper byte order */
     serverAddr.sin_port = htons(portNumber);
     /* Set IP address to localhost */
-    serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    // serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    serverAddr.sin_addr.s_addr = INADDR_ANY;
     /* Set all bits of the padding field to 0 */
     memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);  
 
